6617ac7e76bc644b46ec7b9b3a356b9e026bd04c
rtg <rtg1539@naver.com>
2024-01-31 13:34:28 +0900

    swagger, i18n

--- api/src/main/java/com/web/api/helloworld/HelloWorldController.java
+++ api/src/main/java/com/web/api/helloworld/HelloWorldController.java
@@ -1,11 +1,22 @@
 package com.web.api.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
 
     @GetMapping(path = "/hello")
     public String hello() {
@@ -20,5 +31,12 @@ public class HelloWorldController {
     @GetMapping(path = "/hello/path-variable/{name}")
     public HelloBean helloPathVariable(@PathVariable String name) {
         return new HelloBean(String.format("hello, %s", name));
+    }
+
+    @GetMapping(path = "/hello-i18n")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+
     }
 }
