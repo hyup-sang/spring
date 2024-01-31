e5c01fe487fa1305b8e0e28d49e6b40c5e8f0df3
rtg <rtg1539@naver.com>
2024-01-31 12:11:16 +0900

    crud api

---
+++ api/src/main/java/com/web/api/user/UserNotFoundException.java
@@ -0,0 +1,13 @@
+package com.web.api.user;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+}
