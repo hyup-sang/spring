d0c843ab030f7b8531408646f0db16b9a434c1b1
rtg <rtg1539@naver.com>
2024-02-02 18:35:17 +0900

    CommandLineRunner 구현

--- aop/src/main/java/com/spring/aop/AopApplication.java
+++ aop/src/main/java/com/spring/aop/AopApplication.java
@@ -1,13 +1,28 @@
 package com.spring.aop;
 
+import com.spring.aop.business.BusinessService1;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class AopApplication {
+public class AopApplication implements CommandLineRunner {
+    private BusinessService1 businessService1;
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    public AopApplication(BusinessService1 businessService1) {
+        this.businessService1 = businessService1;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AopApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("Value returned is {}", businessService1.calculateMax());
+    }
 }
