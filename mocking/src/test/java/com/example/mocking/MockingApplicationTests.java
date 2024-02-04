0a21751fa2ae85ce6ce05da9ee94ef8f167ec7bb
rtg <rtg1539@naver.com>
2024-02-04 12:59:26 +0900

    모킹 프로젝트 생성

---
+++ mocking/src/test/java/com/example/mocking/MockingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mocking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
