17f2b9fc17883cb69798bde778c8308293508813
rtg <rtg1539@naver.com>
2024-02-04 13:57:43 +0900

    mock 애너테이션 활용

--- mocking/src/test/java/com/example/mocking/buisness/SomeBusinessImplMockTest.java
+++ mocking/src/test/java/com/example/mocking/buisness/SomeBusinessImplMockTest.java
@@ -1,29 +1,31 @@
 package com.example.mocking.buisness;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-/**
- * 인터페이스에 메서드가 추가 될 때마다 구현 클래스를 업데이트 해야 함
- * 많은 케이스를 테스트하기 어려움
- * */
+@ExtendWith(MockitoExtension.class)
 class SomeBusinessImplMockTest {
+    @Mock
+    private DataService dataServiceMock;
+
+    @InjectMocks
+    private SomeBusinessImpl businessImpl;
+
     @Test
     void findTheGreatestFromAllData_basicScenario() {
-        DataService dataServiceMock = mock(DataService.class);
         when(dataServiceMock.retrieveAllData()).thenReturn(new int[]{25, 15, 5});
-        SomeBusinessImpl businessImpl = new SomeBusinessImpl(dataServiceMock);
         assertEquals(25, businessImpl.findTheGreatestFromAllData());
     }
 
     @Test
     void findTheGreatestFromAllData_OneValue() {
-        DataService dataServiceMock = mock(DataService.class);
         when(dataServiceMock.retrieveAllData()).thenReturn(new int[]{5});
-        SomeBusinessImpl businessImpl = new SomeBusinessImpl(dataServiceMock);
         assertEquals(5, businessImpl.findTheGreatestFromAllData());
     }
 }
